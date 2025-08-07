042d2dedb189f13ca8e91158b6cfe335de088931
Marwa <marwa-aboulkasim@hotmail.fr>
2025-08-07 14:09:44 +0200

    new

--- stroke_api/api.py
+++ stroke_api/api.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter
 from typing import Optional
 import filters
 
+
 router = APIRouter()
 
 @router.get("/")
 def read_root():
     return {"message": "Bienvenue sur l'API Stroke Prediction !"}
 
-<<<<<<< HEAD
 @router.get("/patients/")
 def get_patients(
     gender: Optional[str] = None,
@@ -16,27 +16,16 @@ def get_patients(
     max_age: Optional[float] = None
 ):
     patients = filters.filter_patients(
-=======
-# TODO décommenter et compléter
-@router.get('/patients/')
-def get_patients(gender: str = None, stroke: int = None, max_age: float = None):
-    filtered_df = filters.filter_patient(
-        stroke_data_df=filters.df,
->>>>>>> main
         gender=gender,
         stroke=stroke,
         max_age=max_age
     )
-<<<<<<< HEAD
     return patients.to_dict(orient='records')
 
 
 
 
    
-=======
-    return filtered_df
->>>>>>> main
 
 # TODO décommenter et compléter
 # @router.get("/patients/{patient_id}")
--- stroke_api/main.py
+++ stroke_api/main.py
@@ -3,12 +3,9 @@ import pandas as pd
 import numpy as np
 from api import router
 
-
 # Création d'un objet FastAPI
 app = FastAPI(title="Stroke Dataset API")
 
 # Inclusion des routes définies dans api.py
 app.include_router(router)
-
-
 
