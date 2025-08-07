68ebe9960c05c601cdf987f1a20c840ae09b17da
GaetanCSimplon <gcourtieupro@gmail.com>
2025-08-07 11:46:01 +0200

    test appli api recup données

--- stroke_api/api.py
+++ stroke_api/api.py
@@ -8,14 +8,19 @@ def read_root():
     return {"message": "Bienvenue sur l'API Stroke Prediction !"}
 
 # TODO décommenter et compléter
-#@router.get("/patients/")
-#def get_patients(gender: str = None, stroke: int = None, max_age: float = None):
-    #filtered_df = 
-    #return filtered_df.to_dict(orient="records")
+@router.get('/patients/')
+def get_patients(gender: str = None, stroke: int = None, max_age: float = None):
+    filtered_df = filters.filter_patient(
+        stroke_data_df=filters.df,
+        gender=gender,
+        stroke=stroke,
+        max_age=max_age
+    )
+    return filtered_df
 
 # TODO décommenter et compléter
-#@router.get("/patients/{patient_id}")
+# @router.get("/patients/{patient_id}")
     # Gérer le cas où l'id de patient passé en paramètre n'existe pas
 
 
-# TODO Ajout de la route stats
+#  Ajout de la route stats 
--- stroke_api/filters.py
+++ stroke_api/filters.py
@@ -1,13 +1,26 @@
 from typing import Optional
 import pandas as pd
 
+
 # Chargement des données (une fois)
+df = pd.read_csv('df_final_test.csv')
+
 
 # Tester l'app avec :
 # poetry run fastapi dev stroke_api/main.py
 # http://127.0.0.1:8000/docs : utiliser la fonctionnalité Try it out pour tester les routes
 
 # Ajout des fonctions de filtrage des données cf notebook 1
+
+def filter_patient(stroke_data_df: pd.DataFrame, stroke: Optional[int] = None, gender: Optional[str] = None, max_age: Optional[int] = None):
+    filtered_df = stroke_data_df.copy()
+    if max_age is not None:
+        filtered_df = filtered_df[filtered_df['age'] <= max_age]
+    if stroke is not None:
+        filtered_df = filtered_df[filtered_df['stroke'] == stroke]
+    if gender is not None:
+        filtered_df = filtered_df[filtered_df['gender'] == gender]
+    return filtered_df.to_dict(orient='records')
 # Ensuite faire appel à ces fonctions dans le fichier api.py où sont définies les routes.
 
 # Ajouter les fonctions de filtrage pour les autres routes.
